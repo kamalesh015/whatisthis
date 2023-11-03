2e69af289643a2eb3627cb5cfe4cffd99e0692ba
kamalesh015 <128491236+kamalesh015@users.noreply.github.com>
2023-11-03 16:46:45 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -11,6 +11,12 @@ import torch
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+parser = argparse.ArgumentParser(description="Flask app exposing yolov5 models")
+parser.add_argument("--port", default=5000, type=int, help="port number")
+args = parser.parse_args()
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)  # force_reload = recache latest code
+model.eval()
+app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -37,11 +43,4 @@ def predict():
     return render_template("index.html",img=None)
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Flask app exposing yolov5 models")
-    parser.add_argument("--port", default=5000, type=int, help="port number")
-    args = parser.parse_args()
 
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)  # force_reload = recache latest code
-    model.eval()
-    app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
